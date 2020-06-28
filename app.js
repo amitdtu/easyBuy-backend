bf600dc6a324bb9185fbe19ec1b5c91b619ef35b
amitdtu <amitdtu007@gmail.com>
2020-06-28 19:16:38 +0530

    setup of easyBuy backend

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'everything is perfect' });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const port = process.env.PORT || 8000;
+app.listen(port, 'localhost', () => {
+  console.log(`server is listening on port ${port}`);
+});
